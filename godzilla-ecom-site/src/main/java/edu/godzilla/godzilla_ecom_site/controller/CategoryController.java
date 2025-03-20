3c3492d81633d444a8cfd4d87c51eab58728db9a
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-03-20 09:59:59 +0530

    Created the add new category API

--- godzilla-ecom-site/src/main/java/edu/godzilla/godzilla_ecom_site/controller/CategoryController.java
+++ godzilla-ecom-site/src/main/java/edu/godzilla/godzilla_ecom_site/controller/CategoryController.java
@@ -1,8 +1,7 @@
 package edu.godzilla.godzilla_ecom_site.controller;
 
 import edu.godzilla.godzilla_ecom_site.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +12,14 @@ public class CategoryController {
     private List<Category> categories = new ArrayList<>();
 
     @GetMapping("/api/public/categories")
-    public List<Category> getAllCatagories(){
+    public List<Category> getAllCategories(){
         return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully";
     }
 
 }
